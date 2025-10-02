075e8d86067eaaaf55d1931d7f4d7046e2844e54
BluMew123 <bria.wri.2@gmail.com>
2025-10-02 13:12:04 -0400

    setting up code

---
+++ server.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+let url = 'https://api.api-ninjas.com/v1/sudokugenerate?width=3&height=3&difficulty=medium';
+
+let options = {
+  method: 'GET',
+  headers: {
+    'User-Agent': 'insomnia/11.6.1',
+    'X-Api-Key': '1zekGxh6bYp7FRHq++yg2w==Ujz1EvjPmSt9U9Ab'
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
